afce9fefd0cdb161e57ddafde414265fbd435540
Frederico Martins Porto <martinsporto@gmail.com>
2019-01-31 17:41:42 -0200

    Add the search bar and axios request to app component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Axios from 'axios';
+import SearchBar from './components/SearchBar';
+import ImageList from "./components/ImageList";
 
-const App = () => {
-  return <div>Initial</div>
+class App extends React.Component {
+
+  onSearchSubmit = term => {
+    Axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID a1f844fd1386f89c4b3d4c5d33f998bbd8ec4d50257e10034ed0522fb5d9b957"
+      }
+    });
+  }
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: '20px' }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        <ImageList />
+      </div>
+    );
+  }
 };
 
 ReactDOM.render(<App />, document.querySelector('#root'));
